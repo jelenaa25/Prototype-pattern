225973309144321d2973f4e073f216c5fcf7f4cf
Jelena Beganovic <beganovicjelena@gmail.com>
2022-09-23 14:10:06 +0200

    final commit

---
+++ PPR2/src/Creator/Projektant.java
@@ -0,0 +1,35 @@
+/* Projektant.java
+ * @autor  prof. dr Sinisa Vlajic,
+ * Univerzitet u Beogradu
+ * Fakultet organizacionih nauka 
+ * Katedra za softversko inzenjerstvo
+ * Laboratorija za softversko inzenjerstvo
+ * 06.11.2017
+ */
+
+package Creator;
+
+import AbstractProductA.*;
+import AbstractProductB.*;
+import AbstractProductC.*;
+import Product.*;
+
+public abstract class  Projektant { // Client
+       SoftverskiSistem ss; 
+    
+       abstract EkranskaForma kreirajEkranskuFormu();   
+       abstract BrokerBazePodataka kreirajBrokerBazePodataka ();
+       abstract Kontroler kreirajKontroler (EkranskaForma ef,BrokerBazePodataka bbp);
+       
+       public void Kreiraj()
+        { EkranskaForma ef =kreirajEkranskuFormu();
+          BrokerBazePodataka bbp = kreirajBrokerBazePodataka();
+          Kontroler kon= kreirajKontroler(ef,bbp);
+          ss = new SoftverskiSistem1(ef,bbp,kon);
+          ss.prikaziEkranskuFormu();
+          try { Thread.sleep(3000);} catch (InterruptedException ex) {}
+          try { Thread.sleep(3000);} catch (InterruptedException ex) {}
+          SoftverskiSistem ss1 = ss.Clone();
+          ss1.prikaziEkranskuFormu();
+        }
+}
---
+++ PPR2/src/Product/SoftverskiSistem.java
@@ -0,0 +1,18 @@
+/* SoftverskiSistem.java
+ * @autor  prof. dr Sinisa Vlajic,
+ * Univerzitet u Beogradu
+ * Fakultet organizacionih nauka 
+ * Katedra za softversko inzenjerstvo
+ * Laboratorija za softversko inzenjerstvo
+ * 06.11.2017
+ */
+
+
+package Product;
+
+
+public interface SoftverskiSistem // Prototype
+{
+  void prikaziEkranskuFormu(); 
+  SoftverskiSistem Clone();
+}
